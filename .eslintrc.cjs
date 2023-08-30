9d2a2064eb9e30d2cf05fbf0037deb33262395fd
Alejandro Páez <alepdlb@gmail.com>
2023-08-30 11:22:19 +0200

    feat: install and configure eslint-plugin-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,11 +17,50 @@ module.exports = {
     project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
+    /*
+     * "import/order" sorts the order of import declarations, ie:
+     *
+     * import b from "b";
+     * import { z, a } from "x";
+     */
+    'import/order': [
+      'warn',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          ['external', 'builtin'],
+          ['internal'],
+          ['index', 'sibling', 'parent'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    /*
+     * Then, "sort-imports" complements "import/order" by sorting named imports,
+     * like so:
+     *
+     * import b from "b";
+     * import { a, z } from "x";
+     */
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: true,
+        /*
+         * This option turns off declaration sorting, as this is already handled
+         * by "import/order".
+         */
+        ignoreDeclarationSort: true,
+      },
+    ],
   },
-}
+};
